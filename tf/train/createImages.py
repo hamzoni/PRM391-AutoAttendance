7a61d1b6ba1482ce2d8210f6d7ad5479d76b5525
taquy.se <taquy.se@gmail.com>
2018-03-15 23:31:02 +0700

    create images

---
+++ tf/train/createImages.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+import argparse
+import os
+import os.path
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-f', help='student ID')
+
+args = parser.parse_args()
+
+path = './faces/' + args.f + '/'
+
+if not os.path.exists(path): os.mkdir(path)
+
+cap = cv2.VideoCapture(0)
+
+i = 0;
+while(True):
+	ret, frame = cap.read()
+
+	cv2.waitKey(100);
+	cv2.imshow('frame', frame)
+
+	cv2.imwrite(path + str(i) + '.jpg', frame)
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'): break
+	i = i + 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# crop images
--- tf/train/train.sh
+++ tf/train/train.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python retrain.py --model_dir imagenet --image_dir faces --output_graph faces.graph --how_many_training_steps $1
-
+python3 retrain.py --model_dir imagenet --image_dir faces --output_graph faces.graph --how_many_training_steps $1
